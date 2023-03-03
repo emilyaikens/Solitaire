4b7bb8f9a62c11a0bd05fcc997f3fcc17ff47402
emily.aikens <emily.aikens@orionic.com>
2023-03-03 10:50:00 -0800

    UPDATE wrap instantiation and shuffle in startGame function

--- main.js
+++ main.js
@@ -9,7 +9,6 @@ class Card {
     }
 }
 
-
 // Arrays of links to card images //
 // ----------------------------------------------------------------- //
 let heartImgs = ['','','','','','','','','','','','','']
@@ -18,35 +17,45 @@ let spadeImgs = ['','','','','','','','','','','','','']
 let clubImgs = ['','','','','','','','','','','','','']
 
 
-// Instantiate new cards and push to cards array //
+// Start game //
 // ----------------------------------------------------------------- //
-let cards = []
+function startGame() {
 
-function createCards(suite, color, imgs) {
-    for (let i = 1; i < 14; i++) {
-        cards.push(new Card (suite, i, color, imgs[i-1]))
-    };
+
+    // Instantiate new cards and push to cards array //
+    // ----------------------------------------------------------------- //
+    let cards = []
+
+    function createCards(suite, color, imgs) {
+        for (let i = 1; i < 14; i++) {
+            cards.push(new Card (suite, i, color, imgs[i-1]))
+        };
+    }
+
+    createCards('heart', 'red', heartImgs);
+    createCards('diamond', 'red', diamondImgs);
+    createCards('spade', 'black', spadeImgs);
+    createCards('club', 'black', clubImgs);
+
+
+    // Shuffle cards //
+    // Code based on shuffle function from programiz.com //
+    // ----------------------------------------------------------------- //
+    for (let i = cards.length - 1; i > 0; i--) {
+        let r = Math.floor(Math.random() * i);
+        let temp = cards[i];
+        cards[i] = cards[r];
+        cards[r] = temp;
+    }
+
+    // Dynamically render cards //
+    // ----------------------------------------------------------------- //
+
+
+    // Start timer //
+    // ----------------------------------------------------------------- //
+
+
 }
 
-createCards('heart', 'red', heartImgs);
-createCards('diamond', 'red', diamondImgs);
-createCards('spade', 'black', spadeImgs);
-createCards('club', 'black', clubImgs);
-
-
-// Shuffle cards //
-// Code based on shuffle function from programiz.com //
-// ----------------------------------------------------------------- //
-for (let i = cards.length - 1; i > 0; i--) {
-    let r = Math.floor(Math.random() * i);
-    let temp = cards[i];
-    cards[i] = cards[r];
-    cards[r] = temp;
-}
-
-// Dynamically render cards //
-// ----------------------------------------------------------------- //
-
-
-// Start timer //
-// ----------------------------------------------------------------- //
+startGame(); // <<<<<<<<<<<< START GAME <<<<<<<<<<<< //
