3a406991d610ff35659cbd7a886c29965a371c30
emily.aikens <emily.aikens@orionic.com>
2023-03-03 10:23:23 -0800

    ADD createCard function, called to instantiate cards for each of the four suites

--- main.js
+++ main.js
@@ -9,9 +9,26 @@ class Card {
     }
 }
 
-// list of links to card images //
+
+// array of links to card images //
 // ----------------------------------------------------------------- //
 let heartImgs = ['','','','','','','','','','','','','']
 let diamondImgs = ['','','','','','','','','','','','','']
 let spadeImgs = ['','','','','','','','','','','','','']
 let clubImgs = ['','','','','','','','','','','','','']
+
+
+// instantiate new cards and push to card array //
+// ----------------------------------------------------------------- //
+let cards = []
+
+function createCards(suite, color, imgs) {
+    for (let i = 1; i < 14; i++) {
+        cards.push(new Card (suite, i, color, imgs[i-1]))
+    };
+}
+
+createCards('heart', 'red', heartImgs);
+createCards('diamond', 'red', diamondImgs);
+createCards('spade', 'black', spadeImgs);
+createCards('club', 'black', clubImgs);
